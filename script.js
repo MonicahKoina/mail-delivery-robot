116c0df6f2d65634d871a056a455f425b11f301e
Monicah Koina <monicahkoina1@gmail.com>
2024-08-05 17:03:47 +0300

    road list to a data structure

--- script.js
+++ script.js
@@ -27,3 +27,19 @@ function buildGraph(edges) {
     }
     return graph;
     }
+    //convert roads to a data structure
+    function buildGraph(edges) {
+        let graph = Object.create(null);
+        function addEdge(from, to) {
+        if (from in graph) {
+        graph[from].push(to);
+        } else {
+        graph[from] = [to];
+        }
+        }
+        for (let [from, to] of edges.map(r => r.split("-"))) {
+        addEdge(from, to);
+        addEdge(to, from);
+        }
+        return graph;
+        }
