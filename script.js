0b87b6d32dffc0fe52355a7f0b79a73999ea6b38
Monicah Koina <monicahkoina1@gmail.com>
2024-08-05 17:06:28 +0300

    location update 1

--- script.js
+++ script.js
@@ -43,3 +43,23 @@ function buildGraph(edges) {
         }
         return graph;
         }
+
+        const roadGraph = buildGraph(roads);
+        //addresses and location
+        class VillageState {
+            constructor(place, parcels) {
+            this.place = place;
+            this.parcels = parcels;
+            }
+            move(destination) {
+            if (!roadGraph[this.place].includes(destination)) {
+            return this;
+        } else {
+            let parcels = this.parcels.map(p => {
+            if (p.place != this.place) return p;
+            return {place: destination, address: p.address};
+            }).filter(p => p.place != p.address);
+            return new VillageState(destination, parcels);
+            }
+            }
+            }
